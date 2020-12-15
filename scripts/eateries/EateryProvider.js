5616cd0f0bf1cd17632ee51b7a00def32f01fcdb
sosinatolossa <sosinatolossa16@gmail.com>
2020-12-15 11:06:39 -0600

    worked on data providers

---
+++ scripts/eateries/EateryProvider.js
@@ -0,0 +1,18 @@
+
+let eateries = []
+
+export const copiedEateries = () => {
+    return eateries.slice()
+} 
+
+export const getEateries = () => {
+    return fetch("http://holidayroad.nss.team/eateries")
+    .then (
+        response => response.json())
+        .then (
+            parsedEateries => {
+                console.table(parsedEateries)
+                eateries = parsedEateries
+            }
+        )    
+}
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { getParks } from "./parks/ParkProvider.js"
+import { getEateries } from "./eateries/EateryProvider.js"
+import { getAttractions } from "./attractions/AttractionProvider.js"
+
+getParks()
+getEateries()
+getAttractions()
