ebf82cc85f1197b826e68cfc0285a9bb76d4060e
lcurtis4 <lcurtis0104@gmail.com>
2020-12-15 15:44:46 -0500

    branch

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import { getParks } from "./parks/ParkProvider.js"
 import { getEateries } from "./eateries/EateryProvider.js"
 import { getAttractions } from "./attractions/AttractionProvider.js"
 import { ParkSelect } from "./parks/ParksComponent.js"
+import { EateriesSelect } from "./eateries/eateries.js"
 
 getParks()
 getEateries()
 getAttractions()
 ParkSelect()
+EateriesSelect()
