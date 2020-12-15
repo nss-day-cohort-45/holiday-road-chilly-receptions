376be5a2a702d67bdd3e82dc3fd42bd4b8e7aec1
jessicadever1 <jessicadever1@gmail.com>
2020-12-15 14:27:24 -0600

    added attractions component

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import { getParks } from "./parks/ParkProvider.js"
 import { getEateries } from "./eateries/EateryProvider.js"
 import { getAttractions } from "./attractions/AttractionProvider.js"
 import { ParkSelect } from "./parks/ParksComponent.js"
+import { AttractionSelect } from "./attractions/AttractionsComponent.js"
 
 getParks()
 getEateries()
 getAttractions()
 ParkSelect()
+AttractionSelect()
